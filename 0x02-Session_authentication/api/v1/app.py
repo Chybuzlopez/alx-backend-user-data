8097b91f3c6a6497b3c518be91fc51421ec7f553
Chybuzlopez <danielorihanna@gmail.com>
2023-06-03 20:00:49 +0000

    task done

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -14,26 +14,26 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
-
-if AUTH_TYPE == "auth":
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """ Unauthorized handler
-    """
+def unauthorized(error) -> str:
+    """ error handler for (unauthorized) 401 status code """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden_error(error) -> str:
-    """ Forbidden handler
-    """
+def forbidden(error) -> str:
+    """ error handler for (forbidden) 403 status code """
     return jsonify({"error": "Forbidden"}), 403
 
 
@@ -46,24 +46,21 @@ def not_found(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """ Before Request Handler
-    Requests Validation
-    """
+    """ method to handler before request """
     if auth is None:
         return
-
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if auth.authorization_header(request) is None:
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
         abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python3
-""" 3. Auth class
-"""
-
+""" Auth class, Require auth with stars """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
-class Auth:
-    """ Auth class.
-    """
-
+class Auth():
+    """ manage the API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ def require_auth
-        """
-        if not path or not excluded_paths:
+        """ require authorithation """
+        if path is None or excluded_paths is None or not len(excluded_paths):
             return True
-        path = path + '/' if path[-1] != '/' else path
-        has_wildcard = any(x.endswith("*") for x in excluded_paths)
-        if not has_wildcard:
-            return path not in excluded_paths
-        for e in excluded_paths:
-            if e.endswith("*"):
-                if path.startswith(e[:-1]):
+        if path[-1] != '/':
+            path += '/'
+        for i in excluded_paths:
+            if i.endswith('*'):
+                if path.startswith(i[:1]):
                     return False
-            if path == e:
-                return False
-        return True
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
-        """ def authorization_header
-        """
-        if request:
-            return request.headers.get("Authorization")
+        """ authorization header """
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ def curent_user
-        """
+        """ current user """
         return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request """
+        if request is None:
+            return None
+        session_name = getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
